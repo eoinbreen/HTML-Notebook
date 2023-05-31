a9e7225fdea5533459995173a185c8cc84d631d8
eoinbreen <e.breen185@gmail.com>
2023-05-31 21:56:49 +0100

    Used Slice to make user inputted name into correct format

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -10,8 +10,19 @@ function char_count(){
     alert("You have written " + characters_written + " Characters, you have "+ characters_remaining + " Characters remaining.");
 }
 
-function slice(){
+// Takes the value of a number field on the HTML page and slices the users message to that ammount of characters
+function slice_message(){
     var user_input = prompt("Enter a message that you would like to send : ");
     var number = document.getElementById("slice-max").value;
     alert(user_input.slice(0, number));
+}
+
+//Use Slice to capitalise the first letter in the users input and lowercase the rest
+function capitalise_name(){
+    var name = prompt("What is your Name?");
+    var first_letter = name.slice(0,1);
+    first_letter = first_letter.toUpperCase();
+    rest_of_name = name.slice(1, name.length).toLowerCase()
+    name = first_letter + rest_of_name;
+    alert("Hello " + name + " it's nice to meet you!");
 }
