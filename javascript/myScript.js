4f983f9265524f3f82b2ccf4c07eafbd0ec03a95
eoinbreen <e.breen185@gmail.com>
2023-07-22 15:55:24 +0100

    Added DOM Manipulation

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -243,3 +243,48 @@ function calculator(num1, num2, operator){
     let operator_function = window[operator]; // Allows us to call function using the name in a string
     alert(operator_function(num1, num2));
 }
+
+
+// Style Documentation https://www.w3schools.com/jsref/dom_obj_style.asp
+
+function change_text(){
+    first_link = document.querySelector(".dom-manipulation a")
+    first_link.textContent = "Yahoo"
+    first_link.setAttribute("href", "https://www.yahoo.com")
+    
+    list_link = document.querySelector(".list a");
+    list_link.style.color = "red";
+    
+    second = document.querySelectorAll(".dom-list li")[1];
+    second.innerHTML = "Hello";
+    
+    third = document.firstElementChild.lastElementChild.lastElementChild.lastElementChild;
+    third.textContent = "My Name is Eoin";
+    
+
+}
+
+function checkbox(){
+    var checkbox = document.getElementById("myCheck");
+    var button = document.getElementsByClassName("myButton")[0];
+
+    if(checkbox.checked == true){
+        button.style.backgroundColor = "yellow";
+    }
+    else{
+        button.style.backgroundColor = "red";
+    }
+}
+
+function visibility(){
+    var checkbox = document.querySelector("#visCheck");
+    var button = document.getElementsByClassName("myButton")[0];
+    document.querySelector("h3").classList.toggle("huge");
+
+    if(checkbox.checked == true){
+        button.classList.add("invisible");
+    }
+    else{
+        button.classList.remove("invisible");
+    }
+}
